bad050cc7a0db86bf59f5aa18f42a5dc82c3b346
drtdenis <drouetdenis2@gmail.com>
2021-03-25 16:46:24 +0100

    first commit

---
+++ echo.js
@@ -0,0 +1,13 @@
+if (process.argv.length != 5) {
+  console.log(`usage: node echo.js il faut 3 parametre`)
+  process.exit(1) // exit the program mettre 3 parametre
+}
+//let name = process.argv[2]
+//console.log(`hello hard fork`)
+
+let string = ''
+
+ for(let i = 2 ; i<process.argv.length ; i++) {
+   string += ` ${process.argv[i]}`
+ }
+ console.log(string.trim())
